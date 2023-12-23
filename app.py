bd426b1769671e38f12c3fa876324441c3194261
ssreeramj <ssreeramj@gmail.com>
2023-12-23 17:33:51 +0530

    small formatting

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import logging
-import queue
 import os
+import queue
 import tempfile
 from io import BytesIO
 
@@ -18,6 +18,7 @@ genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 st.set_page_config(page_title="TTT", page_icon="microphone")
 
+
 # Create a function to transcribe audio using Whisper
 def transcribe_audio(audio_file):
     client = OpenAI()
@@ -27,21 +28,22 @@ def transcribe_audio(audio_file):
         temp_filename = temp_file.name  # Get the temporary file name
 
         # Export the audio segment to the temporary file
-        audio_file.export(temp_filename, format="wav")    
+        audio_file.export(temp_filename, format="wav")
 
         # Do something with the temporary file (e.g., process it further)
         print("Temporary file saved at:", temp_filename)
 
     audio_snippet = open(temp_filename, "rb")
-    
+
     # Transcribe the temporary audio file
     transcript = client.audio.translations.create(
-        model="whisper-1", 
+        model="whisper-1",
         file=audio_snippet,
         response_format="text",
     )
 
     return transcript
+
 
 if __name__ == "__main__":
     st.title("Toastmasters Table Topic Master")
@@ -53,13 +55,11 @@ if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
 
-
-
     webrtc_ctx = webrtc_streamer(
         key="audio-video-sendonly",
         mode=WebRtcMode.SENDONLY,
         rtc_configuration={  # Add this config
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+            "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
         },
         media_stream_constraints={"video": True, "audio": True},
     )
@@ -68,7 +68,6 @@ if __name__ == "__main__":
 
     if "raw_video_frames" not in st.session_state:
         st.session_state["raw_video_frames"] = []
-
 
     if "raw_audio" not in st.session_state:
         st.session_state["raw_audio"] = None
@@ -87,7 +86,7 @@ if __name__ == "__main__":
             img_rgb = video_frame.to_ndarray(format="rgb24")
             st.session_state["raw_video_frames"].append(img_rgb)
             image_place.image(img_rgb)
-        
+
         if webrtc_ctx.audio_receiver:
             try:
                 audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
@@ -103,7 +102,11 @@ if __name__ == "__main__":
                     frame_rate=audio_frame.sample_rate,
                     channels=len(audio_frame.layout.channels),
                 )
-                st.write(audio_frame.to_ndarray().shape, audio_frame.sample_rate, len(audio_frame.layout.channels))
+                st.write(
+                    audio_frame.to_ndarray().shape,
+                    audio_frame.sample_rate,
+                    len(audio_frame.layout.channels),
+                )
 
                 sound_chunk += sound
 
@@ -115,9 +118,8 @@ if __name__ == "__main__":
 
                 sound_window_buffer += sound_chunk
 
-
             st.session_state["raw_audio"] = sound_window_buffer
-        
+
         else:
             logger.warning("AudioReciver is not set. Abort.")
             break
@@ -125,10 +127,9 @@ if __name__ == "__main__":
     st.session_state["len_raw_video_frames"] = len(st.session_state["raw_video_frames"])
     st.write(st.session_state)
 
-
     if st.session_state.get("len_raw_video_frames", 0) > 0:
         with st.spinner(text="Generating Feedback"):
-            model = genai.GenerativeModel('gemini-pro-vision')
+            model = genai.GenerativeModel("gemini-pro-vision")
 
             img_prompt = """
             Context: The assistant receives a tiled series of screenshots from a user's live video feed. These screenshots represent sequential frames from the video, capturing distinct moments. The assistant is to analyze these frames as a continuous video feed.
@@ -145,19 +146,19 @@ if __name__ == "__main__":
             pil_image = PIL.Image.fromarray(st.session_state["raw_video_frames"][0])
             st.image(pil_image)
 
-            response = model.generate_content(
-                contents=[img_prompt, pil_image]
-            )
+            response = model.generate_content(contents=[img_prompt, pil_image])
 
             response.resolve()
             st.write(response.text.strip())
 
             # Add the function to the streamlit app
-            
+
             if st.session_state["raw_audio"] is not None:
                 # st.write(audio_frame.to_ndarray().shape, audio_frame.sample_rate, len(audio_frame.layout.channels))
-                st.session_state["exported_audio"] = st.session_state["raw_audio"].export()
+                st.session_state["exported_audio"] = st.session_state[
+                    "raw_audio"
+                ].export()
                 st.audio(st.session_state["exported_audio"].read())
-                transcription = transcribe_audio(st.session_state["raw_audio"])    
+                transcription = transcribe_audio(st.session_state["raw_audio"])
 
                 st.write(transcription)
