f3505ce0e63510cc0672a1e1a804ef547cce85e1
ssreeramj <ssreeramj@gmail.com>
2023-12-25 01:03:18 +0530

    added llm feedback functions

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
+from llm_feedback import generate_text_feedback, generate_video_feedback
+
 load_dotenv()
 
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
@@ -33,25 +35,25 @@ def transcribe_audio(audio_file):
         # Do something with the temporary file (e.g., process it further)
         print("Temporary file saved at:", temp_filename)
 
-    audio_snippet = open(temp_filename, "rb")
-
-    # Transcribe the temporary audio file
-    transcript = client.audio.translations.create(
-        model="whisper-1",
-        file=audio_snippet,
-        response_format="text",
-    )
+    with open(temp_filename, "rb") as audio_snippet:
+        # Transcribe the temporary audio file
+        transcript = client.audio.translations.create(
+            model="whisper-1",
+            file=audio_snippet,
+            response_format="text",
+        )
 
     return transcript
 
 
+def reset_speech():
+    st.session_state["raw_video_frames"] = []
+    st.session_state["raw_audio"] = pydub.AudioSegment.empty()
+    st.session_state["exported_audio"] = None
+
+
 if __name__ == "__main__":
     st.title("Toastmasters Table Topic Master")
-
-    """A sample to use WebRTC in sendonly mode to transfer frames
-    from the browser to the server and to render frames via `st.image`."""
-
-    # from sample_utils.turn import get_ice_servers
 
     logger = logging.getLogger(__name__)
 
@@ -62,15 +64,15 @@ if __name__ == "__main__":
             "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
         },
         media_stream_constraints={"video": True, "audio": True},
+        audio_receiver_size=256,
+        video_receiver_size=256,
     )
 
     image_place = st.empty()
 
     if "raw_video_frames" not in st.session_state:
         st.session_state["raw_video_frames"] = []
-
-    if "raw_audio" not in st.session_state:
-        st.session_state["raw_audio"] = None
+        st.session_state["raw_audio"] = pydub.AudioSegment.empty()
 
     sound_window_len = 500  # 5s
     sound_window_buffer = None
@@ -102,11 +104,6 @@ if __name__ == "__main__":
                     frame_rate=audio_frame.sample_rate,
                     channels=len(audio_frame.layout.channels),
                 )
-                st.write(
-                    audio_frame.to_ndarray().shape,
-                    audio_frame.sample_rate,
-                    len(audio_frame.layout.channels),
-                )
 
                 sound_chunk += sound
 
@@ -119,46 +116,30 @@ if __name__ == "__main__":
                 sound_window_buffer += sound_chunk
 
             st.session_state["raw_audio"] = sound_window_buffer
+            st.session_state["exported_audio"] = sound_window_buffer.export()
 
         else:
             logger.warning("AudioReciver is not set. Abort.")
             break
 
-    st.session_state["len_raw_video_frames"] = len(st.session_state["raw_video_frames"])
     st.write(st.session_state)
 
-    if st.session_state.get("len_raw_video_frames", 0) > 0:
+    # Handling of the video frames and audio buffer
+    if len(st.session_state["raw_audio"]) > 0 and st.session_state["raw_video_frames"]:
         with st.spinner(text="Generating Feedback"):
-            model = genai.GenerativeModel("gemini-pro-vision")
+            st.audio(st.session_state["exported_audio"].read())
 
-            img_prompt = """
-            Context: The assistant receives a tiled series of screenshots from a user's live video feed. These screenshots represent sequential frames from the video, capturing distinct moments. The assistant is to analyze these frames as a continuous video feed.
-
-            1. Address the user directly, and assume that what is shown in the images is what the user is doing.
-            2. Use "you" and "your" to refer to the user.
-            3. DO NOT mention a series of individual images, a strip, a grid, a pattern or a sequence. Do as if the user and the assistant were both seeing the video.
-            4. Keep in mind that the grid of images will show the same object in a sequence of time. E.g. If there are multiple people in different part of the images, it is the same person and NOT multiple people.
-            5. Analyze the video of the person and give them feedback on how to improve their body language with respect to public speaking. Only analyze the video, not the audio
-            6. Tell them what they did right while speaking, and then give them feedback as to what could be improved. 
-            7. Finally conclude your feedback.
-            """
-
-            pil_image = PIL.Image.fromarray(st.session_state["raw_video_frames"][0])
-            st.image(pil_image)
-
-            response = model.generate_content(contents=[img_prompt, pil_image])
-
-            response.resolve()
-            st.write(response.text.strip())
-
-            # Add the function to the streamlit app
-
-            if st.session_state["raw_audio"] is not None:
-                # st.write(audio_frame.to_ndarray().shape, audio_frame.sample_rate, len(audio_frame.layout.channels))
-                st.session_state["exported_audio"] = st.session_state[
-                    "raw_audio"
-                ].export()
-                st.audio(st.session_state["exported_audio"].read())
-                transcription = transcribe_audio(st.session_state["raw_audio"])
-
+            # transcription = transcribe_audio(st.session_state["raw_audio"])
+            with open("./notebooks/sample_text.txt", "r") as f:
+                transcription = f.read()
+            with st.expander(label="Expand to see your speech"):
                 st.write(transcription)
+
+            st.divider()
+
+            video_feedback = generate_video_feedback()
+            text_feedback = generate_text_feedback(
+                speech_text=transcription, topic="Higher Studies"
+            )
+
+    st.button("Reset speech", on_click=reset_speech)
